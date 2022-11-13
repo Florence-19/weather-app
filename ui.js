d30eb210d419272148e107e17efd875b1bc8f660
Florence-19 <94252464+Florence-19@users.noreply.github.com>
2022-11-13 23:31:05 +0100

    api integration
    api integration of weather today from open weather api

--- ui.js
+++ ui.js
@@ -1,5 +1,18 @@
+function showTemperature(response){
+
+    console.log(response.data);
+
+    let temperature = document.querySelector("#col-temp1");
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    
+}
+
+
 let apikey ="52972406f32773a9b6df9fc438d70ab1";
 
-let apiurl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=52972406f32773a9b6df9fc438d70ab1";
+let apiurl = 
+`https://api.openweathermap.org/data/2.5/weather?q=Abuja&appid=52972406f32773a9b6df9fc438d70ab1&units=standard`;
 
-console.log(apiurl);
+ console.log(apiurl);
+
+ axios.get(apiurl).then(showTemperature);
